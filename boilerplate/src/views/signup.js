3ca00958a0a236debcea74730f231b6141fefd72
Peter <78966864+phellwege@users.noreply.github.com>
2023-02-19 13:50:31 -0700

    Update signup.js

--- boilerplate/src/views/signup.js
+++ boilerplate/src/views/signup.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Register from '../components/user/registerUser'
 import { Button, Card, Form, Alert, Container} from 'react-bootstrap';
-import mainLogo from '../static/MainLogo.PNG';
 import '../components/user/login.css';
 
 export default function signup() {
     
     return (
         <div>
-            <div className="behindSignon">
-                <h1>WhatsOn</h1>
-                <div className='behindSignonImgBkg'>
-                    <img src={mainLogo} alt='whatsOn Logo'/>
-                </div>
-            </div>
             <Container className="d-flex align-items-center justify-content-center" 
                 style={{minHeight: '100vh'
                 }}>
