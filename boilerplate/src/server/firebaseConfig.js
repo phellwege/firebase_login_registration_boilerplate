2ba70af8cb5bd395a372497b22b1dcdfe7ded09a
Peter <78966864+phellwege@users.noreply.github.com>
2023-02-20 08:12:34 -0700

    Update firebaseConfig.js

--- boilerplate/src/server/firebaseConfig.js
+++ boilerplate/src/server/firebaseConfig.js
@@ -6,7 +6,7 @@ import { getFirestore, } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
     authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
-    projectId: "whatson-public-beta",
+    projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
     storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
     messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID}`,
     appId: `${process.env.REACT_APP_FIREBASE_APPID}`,
